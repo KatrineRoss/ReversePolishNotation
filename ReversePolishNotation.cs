e9eee7c45d0e2e394c6e47230b0d7539e0e205e0
KatrineRoss <rosyallice@gmail.com>
2016-12-18 14:54:06 +0300

    First commit RPN

---
+++ ReversePolishNotation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotationLib
+{
+    public class ReversePolishNotation
+    {
+        public string RPN(string expression)
+        {
+                Stack<char> texas;
+                string strOut = "";
+                string strIn1 = "";
+                expression += " ";
+                texas = new Stack<char>();
+                for (int i = 0; i < expression.Length; i++)
+                {
+                    if (Char.IsLetter(expression[i]))
+                        strIn1 = "letter";
+                    if (Char.IsNumber(expression[i]) || expression[i] == '.' || expression[i] == ',')
+                        strIn1 = "num";
+                    if (expression[i] == '+' || expression[i] == '-')
+                        strIn1 = "second";
+                    if (expression[i] == '/' || expression[i] == '*' || expression[i] == '^')
+                        strIn1 = "first";
+                    if (expression[i] == '(')
+                        strIn1 = "open_break";
+                    if (expression[i] == ')')
+                        strIn1 = "close_break";
+                    if (expression[i] == ' ')
+                        strIn1 = "empty";
+
+                    switch (strIn1)
+                    {
+                        case "num":
+                        case "letter":
+                            strOut = strOut + expression[i];
+                            break;
+
+                        case "second":
+                            if (texas.Count == 0)
+                            {
+                                texas.Push(expression[i]);
+                                break;
+                            }
+                            else
+                                while (texas.Peek() == '/' || texas.Peek() == '*' || texas.Peek() == '^')
+                                {
+                                    strOut = strOut + texas.Pop();
+
+                                    if (texas.Count == 0)
+                                        break;
+                                    if (texas.Peek() == '-' || texas.Peek() == '+')
+                                        strOut = strOut + texas.Pop();
+                                    if (texas.Count == 0)
+                                        break;
+                                }
+                            texas.Push(expression[i]);
+
+                            if (texas.Count != 0 && texas.Peek() == '(')
+                                texas.Push(expression[i]);
+                            break;
+                        case "first":
+                            texas.Push(expression[i]);
+                            break;
+
+                        case "open_break":
+                            texas.Push(expression[i]);
+                            break;
+
+                        case "close_break":
+                            while (texas.Peek() != '(')
+                                strOut = strOut + texas.Pop();
+                            texas.Pop();
+                            break;
+                        case "empty":
+                            while (texas.Count != 0)
+                            {
+                                strOut = strOut + texas.Pop();
+                            }
+                            break;
+                    }
+                }
+                return strOut;
+            }
+        }
+}
---
+++ ReversePolishNotationTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReversePolishNotationLib;
+
+namespace ReversePolishNotationTests
+{
+    [TestClass]
+    public class ReversePolishNotationTests
+    {
+        [TestMethod]
+        public void RPN_5plus3_53plus()
+        {
+            string expression = "5+3";
+            string expected = "53+";
+
+            ReversePolishNotation r = new ReversePolishNotation();
+            string actual = r.RPN(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RPN_5xI3plus6I_536plusx()
+        {
+            string expression = "5*(3+6)";
+            string expected = "536+*";
+
+            ReversePolishNotation r = new ReversePolishNotation();
+            string actual = r.RPN(expression);
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void RPN_4minus3x7plus1_437xminus1plus()
+        {
+            string expression = "4-3*7+1";
+            string expected = "437*-1+";
+
+            ReversePolishNotation r = new ReversePolishNotation();
+            string actual = r.RPN(expression);
+
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
